3f0e44ec845c6f0933f63ff7aa0c2c7196dd9857
Allie Sterling <sterlina89@gmail.com>
2015-01-28 11:02:45 -0800

    adds show action to books controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 describe BooksController do
-  it 'populates an array of messages' do
-    book = Book.create(title: "The Great Gatsby")
-    get :index
-    expect(assigns(:books)).to match_array [book]
+
+  let(:book) {Book.create(title: "The Great Gatsby")}
+  describe 'GET #index' do
+    it 'populates an array of messages' do
+      get :index
+      expect(assigns(:books)).to match_array [book]
+    end
+
+    it 'renders the #index view' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
-  it 'renders the #index view' do
-    get :index
-    expect(response).to render_template :index
+  describe 'GET #show' do
+    it 'assigns requested book to @book' do
+      get :show, id: book
+      expect(assigns(:book)).to eq book
+    end
+
+    it 'renders the show template' do
+      get :show, id: book
+      expect(response).to render_template :show
+    end
   end
 end
