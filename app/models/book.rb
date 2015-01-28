2182b0a45badfef731e53fae4aa0318ec681667f
Allie Sterling <sterlina89@gmail.com>
2015-01-28 10:13:20 -0800

    adds better_title method to book.rb, taking out the / at the end of book title as data returns

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ActiveRecord::Base
   validates :title, presence: true
+
+  def better_title
+    new_title = / \/ \z/.match(self.title).pre_match
+    self.title = new_title
+    self.save
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -11,4 +11,12 @@ describe Book do
       expect(Book.new(title: nil)).to be_invalid
     end
   end
+
+  describe '#better_title' do
+    it 'reformats title attribute to remove extra punctuation' do
+      book = Book.new(title: "Martin the Warrior / ")
+      book.better_title
+      expect(Book.find(book.id).title).to eq "Martin the Warrior"
+    end
+  end
 end
